a3318b77fecae5939ce320b95f000e2c64fcdbbe
twkhjl <twkhjl@gmail.com>
2025-08-24 21:36:17 +0800

    修改最上方header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Language Learning App </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
